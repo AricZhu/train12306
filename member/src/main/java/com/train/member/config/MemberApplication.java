d37851fa5b2b476c1cb2f88290e6a0c94f0ed0a5
kelin.zrh <2594609013@qq.com>
2024-04-02 21:59:43 +0800

    add mybatis

--- member/src/main/java/com/train/member/config/MemberApplication.java
+++ member/src/main/java/com/train/member/config/MemberApplication.java
@@ -1,5 +1,6 @@
 package com.train.member.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +10,7 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.train")
+@MapperScan("com.train.*.mapper")
 public class MemberApplication {
     public static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
