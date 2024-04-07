81dc8626773ae65442cbef1eb4c2b7382651b0bb
kelin.zrh <2594609013@qq.com>
2024-04-07 15:47:28 +0800

    comment for quartz & schedule

--- batch/src/main/java/com/train/batch/job/QuartzDemo.java
+++ batch/src/main/java/com/train/batch/job/QuartzDemo.java
@@ -1,12 +1,12 @@
-package com.train.batch.job;
-
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-
-public class QuartzDemo implements Job {
-    @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("quartz tic.");
-    }
-}
+//package com.train.batch.job;
+//
+//import org.quartz.Job;
+//import org.quartz.JobExecutionContext;
+//import org.quartz.JobExecutionException;
+//
+//public class QuartzDemo implements Job {
+//    @Override
+//    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+//        System.out.println("quartz tic.");
+//    }
+//}
--- batch/src/main/java/com/train/batch/job/ScheduleDemo.java
+++ batch/src/main/java/com/train/batch/job/ScheduleDemo.java
@@ -1,15 +1,15 @@
-package com.train.batch.job;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-@Component
-@EnableScheduling
-public class ScheduleDemo {
-
-    @Scheduled(cron = "0/5 * * * * ?")
-    private void test() {
-        System.out.println("dida");
-    }
-}
+//package com.train.batch.job;
+//
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@EnableScheduling
+//public class ScheduleDemo {
+//
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    private void test() {
+//        System.out.println("dida");
+//    }
+//}
