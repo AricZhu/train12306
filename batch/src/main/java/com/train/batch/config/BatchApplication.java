9c4e0327967f4cba3ed3c65e8df558e923a64499
kelin.zrh <2594609013@qq.com>
2024-04-08 21:39:07 +0800

    add daily data generator for batch

--- batch/src/main/java/com/train/batch/config/BatchApplication.java
+++ batch/src/main/java/com/train/batch/config/BatchApplication.java
@@ -5,12 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.train")
 @MapperScan("com.train.*.mapper")
+@EnableFeignClients("com.train.batch.feign")
 public class BatchApplication {
     public static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
     public static void main(String[] args) {
